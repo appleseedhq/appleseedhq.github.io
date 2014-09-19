975d994c5c4c06a35bf4c775abeb791aa19efd3c
Francois Beaune <beaune@aist.enst.fr>
2014-09-19 16:37:30 +0200

    only consider the first line of commit messages.

--- js/development-activity.js
+++ js/development-activity.js
@@ -2,19 +2,19 @@ function leftPadWithZero(i) {
     return (i < 10 ? '0' : '') + i
 }
 
-$().ready(function() {
+$().ready(function () {
     $.ajax({
         type: "GET",
         url: "https://api.github.com/repos/appleseedhq/appleseed/commits",
         dataType: "json",
-        success: function(result) {
+        success: function (result) {
             for (var i = 0; i < 5; ++i) {
                 var tempDate        = new Date(result[i].commit.author.date);
                 var commitDate      = tempDate.getFullYear()
                                           + "-" + leftPadWithZero(tempDate.getMonth())
                                           + "-" + leftPadWithZero(tempDate.getDate());
                 var commitAuthor    = result[i].commit.author.name;
-                var commitMessage   = result[i].commit.message;
+                var commitMessage   = result[i].commit.message.split("\n")[0];
                 var commitUrl       = result[i].html_url;
 
                 $("#latest-commits").append(
