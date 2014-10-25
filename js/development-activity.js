6283683f0319b95f8eb627068a06feab61b8ce4f
Francois Beaune <beaune@aist.enst.fr>
2014-10-25 21:22:44 +0200

    cosmetics.

--- js/development-activity.js
+++ js/development-activity.js
@@ -24,7 +24,7 @@ $().ready(function () {
                         commitMessage +
                         "</a></li>"
                 );
-                
+
                 $("#commits").show();
             }
         }
